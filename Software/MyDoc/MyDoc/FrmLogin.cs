558fa352eaa72e602fc876b7aa7ece935133dc56
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2024-06-02 21:52:57 +0200

    Finalni projekt

---
+++ Software/MyDoc/MyDoc/FrmLogin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace MyDoc
+{
+    public partial class FrmLogin : Form
+    {
+        string username = "administrator";
+        string password = "lsuto22";
+        public FrmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                MessageBox.Show("Korisničko ime nije uneseno!", "Pogreška",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (txtPassword.Text == "")
+            {
+                MessageBox.Show("Lozinka nije unesena!", "Pogreška", MessageBoxButtons.OK,
+                MessageBoxIcon.Error);
+            }
+            else
+            {
+                if (txtUsername.Text == username && txtPassword.Text == password)
+                {
+                    MessageBox.Show("Uspješno ste prijavljeni!", "Dobrodošli!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    FrmDoctors frmdoctors = new FrmDoctors();
+                    Hide();
+                    frmdoctors.ShowDialog();
+                    Close();
+                }
+                else
+                {
+                    MessageBox.Show("Pogrešno uneseni podaci!", "Pogreška", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
---
+++ Software/MyDoc/MyDoc/Repositories/DoctorRepository.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBLayer;
+using MyDoc.Models;
+
+namespace MyDoc.Repositories
+{
+    public class DoctorRepository
+    {
+        public static Doctor GetDoctor(int id)
+        {
+            Doctor doctor = null;
+            string sql = $"SELECT * FROM Doctors WHERE Id = {id}";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            if (reader.HasRows)
+            {
+                reader.Read();
+                doctor = CreateObject(reader);
+                reader.Close();
+            }
+            DB.CloseConnection();
+            return doctor;
+        }
+        public static List<Doctor> GetDoctors()
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            string sql = "SELECT * FROM Doctors";
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Doctor doctor = CreateObject(reader);
+                doctors.Add(doctor);
+            }
+            reader.Close();
+            DB.CloseConnection();
+            return doctors;
+        }
+        private static Doctor CreateObject(SqlDataReader reader)
+        {
+            int id = int.Parse(reader["ID_Lijecnik"].ToString());
+            string firstAndLastName = reader["Ime_prezime"].ToString();
+            string specialization = reader["Specijalizacija"].ToString();
+            string contact = reader["Kontakt"].ToString();
+            string location = reader["Lokacija"].ToString();
+            string availability = reader["Dostupnost"].ToString();
+            var doctor = new Doctor
+            {
+                Id = id,
+                FirstAndLastName = firstAndLastName,
+                Specialization = specialization,
+                Contact = contact,
+                Location = location,
+                Availability = availability
+            };
+            return doctor;
+        }
+        public static bool DeleteDoctorAttributes(int doctorId)
+        {
+            string sql = $"DELETE FROM Doctors WHERE ID_Lijecnik = {doctorId}";
+
+            DB.OpenConnection();
+
+            bool success = DB.ExecuteCommand(sql) > 0;
+
+            DB.CloseConnection();
+
+            return success;
+        }
+        public static List<Doctor> GetDoctorsByLocation(string location)
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            string sql = $"SELECT * FROM Doctors WHERE Lokacija = '{location}'";
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Doctor doctor = CreateObject(reader);
+                doctors.Add(doctor);
+            }
+            reader.Close();
+            DB.CloseConnection();
+
+            return doctors;
+        }
+        public static List<Doctor> GetDoctorsByAvailability(string availability)
+        {
+            List<Doctor> doctors = new List<Doctor>();
+            string sql = $"SELECT * FROM Doctors WHERE Dostupnost = '{availability}'";
+
+            DB.OpenConnection();
+            var reader = DB.GetDataReader(sql);
+            while (reader.Read())
+            {
+                Doctor doctor = CreateObject(reader);
+                doctors.Add(doctor);
+            }
+            reader.Close();
+            DB.CloseConnection();
+
+            return doctors;
+        }
+
+        public static Doctor GetDoctorById(int doctorId)
+        {
+            string sql = $"SELECT * FROM Doctors WHERE ID_lijecnik = {doctorId}";
+
+            DB.OpenConnection();
+            Doctor doctor = null;
+
+            using (var reader = DB.GetDataReader(sql))
+            {
+                if (reader.Read())
+                {
+                    doctor = new Doctor
+                    {
+                        Id = (int)reader["ID_lijecnik"],
+                        FirstAndLastName = reader["Ime_prezime"].ToString(),
+                        Specialization = reader["Specijalizacija"].ToString(),
+                        Contact = reader["Kontakt"].ToString(),
+                        Location = reader["Lokacija"].ToString(),
+                        Availability = reader["Dostupnost"].ToString()
+                    };
+                }
+                reader.Close();
+            }
+
+            DB.CloseConnection();
+            return doctor;
+        }
+
+        
+        public static void InsertDoctor(Doctor doctor)
+        {
+            string sql = $"INSERT INTO Doctors (ID_lijecnik, Ime_prezime, Specijalizacija, Kontakt, Lokacija, Dostupnost) " +
+                         $"VALUES ({doctor.Id}, '{doctor.FirstAndLastName}', '{doctor.Specialization}', '{doctor.Contact}', '{doctor.Location}', '{doctor.Availability}')";
+
+            DB.OpenConnection();
+            DB.ExecuteCommand(sql);
+            DB.CloseConnection();
+        }
+        public static void UpdateDoctor(Doctor doctor)
+        {
+            string sql = $"UPDATE Doctors SET Ime_prezime = '{doctor.FirstAndLastName}', Specijalizacija = '{doctor.Specialization}', Kontakt = '{doctor.Contact}', Lokacija = '{doctor.Location}', Dostupnost = '{doctor.Availability}' WHERE ID_lijecnik = {doctor.Id} ";
+
+            DB.OpenConnection();
+            DB.ExecuteCommand(sql);
+            DB.CloseConnection();
+        }
+        public static bool IfDoctorIdExists(int doctorId)
+        {
+            string sql = $"SELECT * FROM Doctors WHERE ID_lijecnik = {doctorId}";
+
+            DB.OpenConnection();
+
+            using (var reader = DB.GetDataReader(sql))
+            {
+                if (reader.Read())
+                {
+                    reader.Close();
+                    DB.CloseConnection();
+                    return true;
+                }
+                else
+                {
+                    reader.Close();
+                    DB.CloseConnection();
+                    return false;
+                }
+            }
+
+        }
+
+    }
+}
