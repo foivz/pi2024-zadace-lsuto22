f8436f4b9d3ac18f806aa6fc775fb7bb2ec4a39b
lsuto22 <162358308+lsuto22@users.noreply.github.com>
2024-06-02 21:51:21 +0200

    Dodan projekt
    finalni projekt

---
+++ Software/MyDoc/MyDoc/FrmAddDoctor.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyDoc.Models;
+using MyDoc.Repositories;
+
+namespace MyDoc
+{
+    public partial class FrmAddDoctor : Form
+    {
+        public FrmAddDoctor()
+        {
+            InitializeComponent();
+        }
+
+        private void AddDoctor_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPovratak_Click(object sender, EventArgs e)
+        {
+            FrmDoctors frmdoctors = new FrmDoctors();
+            Hide();
+            frmdoctors.ShowDialog();
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtDoctorID_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtFirstAndLastName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSpecialization_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtContact_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLocation_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAvailability_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+
+                int doctorID;
+                if (!int.TryParse(txtDoctorID.Text, out doctorID))
+                {
+                    MessageBox.Show("Molimo popunite sva polja!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (DoctorRepository.IfDoctorIdExists(doctorID))
+                {
+                    MessageBox.Show("Liječnik s unesenim ID-om već postoji!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                string firstAndLastName = txtFirstAndLastName.Text;
+                string specialization = txtSpecialization.Text;
+                string contact = txtContact.Text;
+                string location = txtLocation.Text;
+                string availability = txtAvailability.Text;
+
+
+                if (string.IsNullOrWhiteSpace(firstAndLastName) || string.IsNullOrWhiteSpace(specialization) ||
+                    string.IsNullOrWhiteSpace(contact) || string.IsNullOrWhiteSpace(location) || string.IsNullOrWhiteSpace(availability))
+                {
+                    MessageBox.Show("Molimo popunite sva polja!", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+
+                Doctor noviLijecnik = new Doctor
+                {
+                    Id = doctorID,
+                    FirstAndLastName = firstAndLastName,
+                    Specialization = specialization,
+                    Contact = contact,
+                    Location = location,
+                    Availability = availability
+                };
+
+
+                DoctorRepository.InsertDoctor(noviLijecnik);
+
+                MessageBox.Show("Novi liječnik je uspješno dodan!", "Uspješno dodavanje liječnika!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
---
+++ Software/MyDoc/MyDoc/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDoc.Models
+{
+    public abstract class User : object
+    {
+        public int Id { get; set; }
+        public string FirstAndLastName { get; set; }
+        public string Specialization { get; set; }
+        public string Contact { get; set; }
+        public string Location { get; set; }
+        public string Availability { get; set; }
+        public override string ToString()
+        {
+            return FirstAndLastName + " " + Specialization + " " + Contact + " " + Location + " " + Availability;
+        }
+    }
+
+}
